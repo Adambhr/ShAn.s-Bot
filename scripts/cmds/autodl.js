01124963d13bfbcb58901c1d2c3020383b83494c
KABOS-Dz <Adambhr143@gmail.com>
2025-09-25 13:33:25 +0100

    Update autodl.js

--- scripts/cmds/autodl.js
+++ scripts/cmds/autodl.js
@@ -101,7 +101,7 @@ module.exports.onChat = async ({ api, event }) => {
     const videoStream = await axios.get(downloadUrl, { responseType: "stream" });
     api.sendMessage(
       {
-        body: `✅ Successfully downloaded the video!\n🔖 Platform: ${platform}\n😜Power by Ew'r ShAn's😪`,
+        body: `✅ Successfully downloaded the video!\n🔖 Platform: ${platform}\n😜Power by kabos 😪`,
         attachment: [videoStream.data],
       },
       threadID,
